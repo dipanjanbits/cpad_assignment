2471d40b0a9e3aa9f7658469c30688269ca4e596
Dipanjan Biswas <dipanjan561@gmail.com>
2022-04-30 21:01:18 +0530

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = "0x!b#(1*cd73w$&azzc6p+essg7v=g80ls#z&xcx*mpemx&@9$"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.compute.amazonaws.com','.ap-south-1.elasticbeanstalk.com']
+#ALLOWED_HOSTS = ['.compute.amazonaws.com','.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
